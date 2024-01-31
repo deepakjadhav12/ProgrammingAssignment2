5027f88971e130a2a2ef30b933f1c33e209ae9e6
Deepak Jadhav <112792282+deepakjadhav12@users.noreply.github.com>
2024-01-31 15:43:07 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,68 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# Function to create a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  
+  # Setter function to set the matrix
+  setMatrix <- function(matrix) {
+    x <<- matrix
+    inv <<- NULL  # Invalidate the cached inverse
+  }
+  
+  # Getter function to get the matrix
+  getMatrix <- function() {
+    x
+  }
+  
+  # Setter function to set the inverse
+  setInverse <- function(inverse) {
+    inv <<- inverse
+  }
+  
+  # Getter function to get the inverse
+  getInverse <- function() {
+    inv
+  }
+  
+  # Return a list of functions
+  list(setMatrix = setMatrix, getMatrix = getMatrix, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# Function to compute the inverse of the special "matrix" and cache it
+cacheSolve <- function(cacheMatrix) {
+  # Retrieve the cached inverse
+  inv <- cacheMatrix$getInverse()
+  
+  # If the inverse is already computed, return it
+  if (!is.null(inv)) {
+    message("Getting cached data")
+    return(inv)
+  }
+  
+  # If the inverse is not cached, compute it using solve
+  mat <- cacheMatrix$getMatrix()
+  inv <- solve(mat)
+  
+  # Cache the computed inverse
+  cacheMatrix$setInverse(inv)
+  
+  inv
 }
+
+# Example usage:
+# Create a special matrix object
+matObj <- makeCacheMatrix(matrix(c(4, 2, 1, 1, 7, 5, 2, 5, 11), nrow = 3, byrow = TRUE))
+
+# Retrieve and print the original matrix
+originalMatrix <- matObj$getMatrix()
+print("Original Matrix:")
+print(originalMatrix)
+
+# Compute and cache the inverse
+cachedInverse <- cacheSolve(matObj)
+print("Computed Inverse:")
+print(cachedInverse)
+
+# Retrieve the cached inverse
+cachedInverse <- cacheSolve(matObj)
+print("Retrieved Cached Inverse:")
+print(cachedInverse)
